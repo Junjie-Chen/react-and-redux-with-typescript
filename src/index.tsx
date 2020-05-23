b92d3fdac831125598c046cda81cfc1f8b866e56
Junjie Chen <junjie.chen18@gmail.com>
2020-05-23 16:03:10 -0400

    feat(entry): render App component in browser

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { connectedApp as App } from './components/App';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
