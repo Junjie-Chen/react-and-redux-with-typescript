5ae7bd97a97671947d1f5c22fb27e0aab7b43e83
Junjie Chen <junjie.chen18@gmail.com>
2020-05-23 16:42:01 -0400

    feat(store): pull state from store

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ import { connectedApp as App } from './components/App';
 const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
