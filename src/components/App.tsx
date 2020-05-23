86cf42f703642a3dc8409adf718d69f10b03a54b
Junjie Chen <junjie.chen18@gmail.com>
2020-05-23 17:20:20 -0400

    feat(components): create mapDispatchToProps function

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,19 @@
+import { loadingTodos, fetchTodos, deleteTodo } from '../actions';
 import { State } from '../reducers';
 
 const mapStateToProps = (state: State): State => ({
   loading: state.loading,
   todos: state.todos
+});
+
+const mapDispatchToProps = dispatch => ({
+  loadingTodos(loading: boolean): void {
+    dispatch(loadingTodos(loading));
+  },
+  fetchTodos(): void {
+    dispatch(fetchTodos());
+  },
+  deleteTodo(id: number): void {
+    dispatch(deleteTodo(id));
+  }
 });
