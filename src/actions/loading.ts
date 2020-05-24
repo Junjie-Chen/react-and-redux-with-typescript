14d5c6dfd332faaa42e67d2fcb58b7f4c94cfb98
Junjie Chen <junjie.chen18@gmail.com>
2020-05-23 21:11:58 -0400

    feat(actions): create loadingTodos action creator

--- src/actions/loading.ts
+++ src/actions/loading.ts
@@ -4,3 +4,8 @@ export interface LoadingTodos {
   type: Types.LoadingTodos;
   payload: boolean;
 }
+
+export const loadingTodos = (loading: boolean) => ({
+  type: Types.LoadingTodos,
+  payload: loading
+});
