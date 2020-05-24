00a72ec160d2c57ff78ff0eba62447792013c02b
Junjie Chen <junjie.chen18@gmail.com>
2020-05-23 21:09:12 -0400

    feat(actions): create Action type

--- src/actions/types.ts
+++ src/actions/types.ts
@@ -1,5 +1,10 @@
+import { LoadingTodos } from './loading';
+import { FetchTodos, DeleteTodo } from './todos';
+
 export enum Types {
   LoadingTodos,
   FetchTodos,
   DeleteTodo
 }
+
+export type Action = LoadingTodos | FetchTodos | DeleteTodo;
