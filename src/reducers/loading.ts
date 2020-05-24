6a927a4c8b2568a6d325d20df87edbd139a15f60
Junjie Chen <junjie.chen18@gmail.com>
2020-05-23 22:27:26 -0400

    feat(reducers): create loading reducer

--- src/reducers/loading.ts
+++ src/reducers/loading.ts
@@ -1,5 +1,16 @@
+import { Types } from '../actions';
+
 export interface Loading {
   loadingTodos: boolean;
 }
 
 const loadingState = { loadingTodos: false };
+
+export const loading = (state = loadingState, action) => {
+  switch (action.type) {
+    case Types.LoadingTodos:
+      return { ...state, loadingTodos: action.payload };
+    default:
+      return state;
+  }
+};
