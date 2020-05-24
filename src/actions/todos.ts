aaa1aad0c8926ab6558570325f70e971f6b62bd5
Junjie Chen <junjie.chen18@gmail.com>
2020-05-23 21:25:56 -0400

    feat(actions): create FetchTodos interface

--- src/actions/todos.ts
+++ src/actions/todos.ts
@@ -1,5 +1,12 @@
+import { Types } from '.';
+
 export interface Todo {
   id: number;
   title: string;
   completed: boolean;
+}
+
+export interface FetchTodos {
+  type: Types.FetchTodos;
+  payload: Todo[];
 }
